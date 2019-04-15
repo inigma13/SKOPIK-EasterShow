6ecaa3994ae076d8e71316796e72ddc1f162a2a3
inigma13 <46365188+inigma13@users.noreply.github.com>
2019-04-15 16:36:43 +1200

    IMG responsiveness done

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html>
 <body>
 <link rel="stylesheet" href="index.css">
-<img src="images/background.jpg" class="responsive" width="600" height="400">
+<img src="images/background.jpg" alt="SKOPIK" class="responsive" width="1080" height="1080">
 </body>
 </html>
 
