acf9915c7b95ff63130a04ea01b43ac95e28eb96
inigma13 <46365188+inigma13@users.noreply.github.com>
2019-04-16 13:52:46 +1200

    Facebook like button added

--- index.php
+++ index.php
@@ -2,8 +2,21 @@
 ?>
 <html>
 <body>
+<script>
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId            : '2309654015919528',
+            autoLogAppEvents : true,
+            xfbml            : true,
+            version          : 'v3.2'
+        });
+    };
+</script>
+<script async defer src="https://connect.facebook.net/en_US/sdk.js"></script>
 <link rel="stylesheet" href="index.css">
-<img src="images/background.jpg" alt="SKOPIK" class="responsive" width="1080" height="1080">
-</body>
+<div style="text-align: center;">
+    <img src="images/background.jpg" alt="SKOPIK" class="responsive" width="1080" height="1080"><br>
+<div class="fb-like" data-href="https://www.facebook.com/skopiknz" data-width="600" data-layout="button" data-action="like" data-size="large" data-show-faces="false" data-share="false"></div>
+</div>
 </html>
 
