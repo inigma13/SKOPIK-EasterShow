2ed92ccc1359189256c83c828bae757a0d0de4ef
inigma13 <46365188+inigma13@users.noreply.github.com>
2019-04-17 10:40:38 +1200

    prefinal

--- Welcome.php
+++ Welcome.php
@@ -11,13 +11,15 @@ window.fbAsyncInit = function() {
             version          : 'v3.2'
         });
     };
-}
 </script>
-<script async defer src="https://connect.facebook.net/en_US/sdk.js"></script>
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v3.2&appId=2309654015919528&autoLogAppEvents=1"></script>
 <link rel="stylesheet" href="index.css">
 <div style="text-align: center;">
     <img src="images/background.jpg" alt="SKOPIK" class="responsive" width="1080" height="1080"><br>
-    <h1 id="continue" onclick="redirect()"><< CONTINUE >></h1>
+    <h1>1. PLEASE LIKE OUR FACEBOOK PAGE</h1> <div class="fb-like" data-href="https://www.facebook.com/skopiknz" data-width="600" data-layout="button" data-action="like" data-size="large" data-show-faces="false" data-share="false"></div>
+
+    <br><h1 id="continue" onclick="redirect()">2. CLICK << CONTINUE >></h1>
     <script>
         function redirect() {
             document.getElementById("continue").innerHTML = "Please Wait...";
@@ -27,9 +29,9 @@ window.fbAsyncInit = function() {
             var ipAddy = url.searchParams.get("ip");
             window.location.href = "AuthAndRedirect.php?mac=" + macAddy + "&ip=" + ipAddy;
         }
-
     </script>
-<div class="fb-like" data-href="https://www.facebook.com/skopiknz" data-width="600" data-layout="button"
-data-action="like" data-size="large" data-show-faces="false" data-share="false"></div>
+
 </div>
+
+
 </html>
