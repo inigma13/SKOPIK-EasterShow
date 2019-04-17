a9b61821de3dcda1c5d49313169eba2aa1b910bf
inigma13 <46365188+inigma13@users.noreply.github.com>
2019-04-17 16:51:48 +1200

    final?

--- Welcome.php
+++ Welcome.php
@@ -2,6 +2,7 @@
 ?>
 <html>
 <body>
+<link rel="stylesheet" href="index.css">
 <script>
 window.fbAsyncInit = function() {
     FB.init({
@@ -14,12 +15,31 @@ window.fbAsyncInit = function() {
 </script>
 <div id="fb-root"></div>
 <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v3.2&appId=2309654015919528&autoLogAppEvents=1"></script>
-<link rel="stylesheet" href="index.css">
-<div style="text-align: center;">
-    <img src="images/background.jpg" alt="SKOPIK" class="responsive" width="1080" height="1080"><br>
-    <h1>1. PLEASE LIKE OUR FACEBOOK PAGE</h1> <div class="fb-like" data-href="https://www.facebook.com/skopiknz" data-width="600" data-layout="button" data-action="like" data-size="large" data-show-faces="false" data-share="false"></div>
 
-    <br><h1 id="continue" onclick="redirect()">2. CLICK << CONTINUE >></h1>
+    <div style="text-align: center;">
+    <img src="images/background.jpg" alt="SKOPIK" class="responsive" width="1080" height="1080"><br>
+    <h1>1. LIKE OUR FACEBOOK PAGE</h1>
+    <div id="step1" class="center"><tr>
+        <td>
+            <img style="text-align: center;" alt="arrow" class="imgResponsive" src="images/leftarrow.png"/></td>
+        <td style="padding-bottom: 100px">
+            <div class="fb-like" data-href="https://www.facebook.com/skopiknz" data-width="600" data-layout="button"
+                 data-action="like" data-size="large" data-show-faces="false" data-share="false"></div>
+        </td>
+        <td>
+            <img style="text-align: center;" alt="arrow" class="imgResponsive" src="images/arrow.png"/>
+        </td>
+    </tr></div>
+    <div id="step2" class="center">
+       <tr><td>
+                <h1 id="continue">2. </h1>
+           </td>
+           <td>
+                   <button class="login100-form-btn" onclick="redirect()">CLICK HERE TO CONNECT</button>
+       </td></tr>
+    </div>
+
+
     <script>
         function redirect() {
             document.getElementById("continue").innerHTML = "Please Wait...";
@@ -30,8 +50,6 @@ window.fbAsyncInit = function() {
             window.location.href = "AuthAndRedirect.php?mac=" + macAddy + "&ip=" + ipAddy;
         }
     </script>
-
 </div>
-
-
+</body>
 </html>
